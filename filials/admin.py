816369e137ad6fbdaf00455c747de55017ce23bf
rita <margaritachka@gmail.com>
2016-05-17 10:25:49 +0300

    created new apps: filials and trainers

---
+++ filials/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Filial
+
+class FilialAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Filial
+
+	list_display = ['city', 'address', 'email', 'phone']
+	list_editable = ['city', 'address', 'email', 'phone']
+
+admin.site.register(Filial, FilialAdmin)
