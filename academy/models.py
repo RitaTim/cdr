3f5030d6c12472f453b320f379f5b5c6a1bb2075
rita <margaritachka@gmail.com>
2016-05-24 22:06:29 +0300

    Create app academy. Coding page academy

--- academy/models.py
+++ academy/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 from django.db import models
 
+def get_uploaded_file_name(instance, filename):
+	return "images/academy/%s" % filename
+
 class Academy(models.Model):
 	title = models.CharField(verbose_name="Заголовок", max_length=400)
 	description = models.TextField(verbose_name="Описание", blank=True, null=True)
@@ -17,6 +20,7 @@ class Academy(models.Model):
 class Section(models.Model):
 	title = models.CharField(verbose_name="Название раздела", max_length=400)
 	description = models.TextField(verbose_name="Описание", blank=True, null=True)
+	img = models.ImageField(upload_to=get_uploaded_file_name, verbose_name="Картинка", blank=True, null=True)
 	updated = models.DateTimeField(verbose_name="Данные обновлены", auto_now=True, auto_now_add=False)
 	created = models.DateTimeField(verbose_name="Данные созданы", auto_now=False, auto_now_add=True)
 
